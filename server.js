cd9f66ed338177d1c5b1122a389b174b20128368
Tam Phan <tnphan0204@gmail.com>
2019-07-12 21:22:30 -0400

    Added search for genres, country, avg_vote

--- server.js
+++ server.js
@@ -1,1 +1,51 @@
-console.log('hello')
+require('dotenv').config()
+const express = require('express')
+const morgan = require('morgan')
+const helmet = require('helmet')
+const MOVIEDEX = require('./moviedex.json')
+console.log(process.env.API_TOKEN)
+
+const app = express()
+
+app.use(morgan('dev'))
+app.use(helmet())
+app.use(function validateBearerToken(req, res, next) {
+    const apiToken = process.env.API_TOKEN
+    const authToken = req.get('Authorization')
+    console.log('validate bearer token middleware')
+    
+    if(!authToken || authToken.split(' ')[1] !== apiToken) {
+        return res.status(401).json({ error: 'Unauthorized request'})
+    }
+    
+    next()
+})
+
+
+app.get('/movies', function handleGetMovies(req, res) {
+    let response = MOVIEDEX;
+
+    if(req.query.genres) {
+        response = response.filter(movie =>
+            movie.genre.toLowerCase().includes(req.query.genres.toLowerCase())
+            )
+    }
+
+    if(req.query.country) {
+        response = response.filter(movie => 
+            movie.country.toLowerCase().includes(req.query.country.toLowerCase())
+        )
+    }
+
+    if(req.query.avg_vote) {
+        response = response.filter(movie =>
+            Number(movie.avg_vote) >= Number(req.query.avg_vote))
+    }
+   res.json(response)
+})
+
+const PORT = 8000
+
+app.listen(PORT, () => {
+    console.log(`Server listening at http://localhost:${PORT}`)
+})
